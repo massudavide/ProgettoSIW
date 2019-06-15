4a4004ba64ef9f372b69b0230d16eecdeda6b6cd
francoMarini <francomarini97@hotmail.com>
2019-06-15 11:17:52 +0200

    Aggiunta la classe FotoForm e completata fotoForm.html

---
+++ progettoSIW/src/main/java/it/uniroma3/siw/progettoSIW/services/FotoForm.java
@@ -0,0 +1,31 @@
+package it.uniroma3.siw.progettoSIW.services;
+
+public class FotoForm {
+	private String titolo; 
+	private String url; 
+	private String fotografoId;
+	
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public String getFotografoId() {
+		return fotografoId;
+	}
+	public void setFotografoId(String fotografoId) {
+		this.fotografoId = fotografoId;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+
+	
+
+}
